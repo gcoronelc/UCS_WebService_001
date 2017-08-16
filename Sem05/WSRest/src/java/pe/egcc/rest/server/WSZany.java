6e4a750c911772c172ba25bbcf75705f7884f066
Gustavo Coronel <gcoronelc@gmail.com>
2017-08-15 20:18:26 -0500

    Sem 05

--- Sem05/WSRest/src/java/pe/egcc/rest/server/WSZany.java
+++ Sem05/WSRest/src/java/pe/egcc/rest/server/WSZany.java
@@ -2,6 +2,7 @@ package pe.egcc.rest.server;
 
 import java.util.List;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
